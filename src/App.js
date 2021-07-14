9217f6aa143d165283e1558eb68f17c016be81df
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2021-07-14 15:37:04 +0800

    Events page, child events

--- src/App.js
+++ src/App.js
@@ -10,13 +10,23 @@ class App extends React.Component {
     this.updateState = this.updateState.bind(this);
   };
   updateState() {
-    this.setState({ data: 'Data updated...' })
+    this.setState({ data: 'Data updated from the child component...' })
   }
   render() {
     return (
       <div>
-        <button onClick={this.updateState}>CLICK</button>
-        <h4>{this.state.data}</h4>
+        <Content myDataProp={this.state.data}
+          updateStateProp={this.updateState}></Content>
+      </div>
+    );
+  }
+}
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.updateStateProp}>CLICK</button>
+        <h3>{this.props.myDataProp}</h3>
       </div>
     );
   }
